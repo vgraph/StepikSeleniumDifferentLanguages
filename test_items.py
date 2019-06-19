d5fe4ce649011031fd50b633da42dcd1b2208fef
vgraph <vadim@vgraph.ru>
2019-06-19 18:38:27 +0300

    Реализован выбор языка интерфейса браузера

--- test_items.py
+++ test_items.py
@@ -1,7 +1,3 @@
-import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
@@ -15,6 +11,10 @@ class TestProductPage:
         """
         # Открываем страницу товара
         browser.get(link)
+        
+        # Установлено время принудительной задержки браузера
+        # после открытия страницы, для визуальной проверки языка открытой страницы
         time.sleep(5)
+        
         # Проверяем наличие кнопки добавления товара в корзину
         assert browser.find_element_by_css_selector("button.btn-add-to-basket")
