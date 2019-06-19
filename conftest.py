56b56cb5e450a3332bb4f55cb298973274711839
vgraph <vadim@vgraph.ru>
2019-06-19 17:05:58 +0300

    Добавляем Options()

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
 def browser():
+    options = Options()
+    
     browser = webdriver.Chrome()
     browser.implicitly_wait(5)
     yield browser
