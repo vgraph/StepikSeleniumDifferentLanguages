4bb17977ebff174469033d9db784deedd7e73f9e
vgraph <vadim@vgraph.ru>
2019-06-19 16:55:44 +0300

    Добавлена базовая проверка кнопки добавления товара.

---
+++ test_items.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+class TestProductPage:
+    
+    def test_button_add_to_basket_is_visible(self, browser):
+        """Тест проверяет, что страница товара
+         содержит кнопку добавления в корзину
+        """
+        # Открываем страницу товара
+        browser.get(link)
+        # Проверяем наличие кнопки добавления товара в корзину
+        assert browser.find_element_by_css_selector("button.btn-add-to-basket")
